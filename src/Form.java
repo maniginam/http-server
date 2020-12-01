904273089fe58e17ad291a270751a6724651488b
maniginam <maniginam@gmail.com>
2020-12-01 17:31:18 -0600

    one very ugly but working forms spec

--- src/Form.java
+++ src/Form.java
@@ -1,25 +1,25 @@
-public class FormManager {
+public class Form {
 
     private final String tailResponse;
     private String response;
-    private String[] inputBoxes;
+    private String[] boxes;
     private String[] boxText;
 
-    public FormManager() {
-        String[] inputBoxes = new String[2];
-        inputBoxes[0] = "Foo";
-        inputBoxes[1] = "Bar";
+    public Form(String[] boxes) {
+        this.boxes = boxes;
         response = "<html>\r\n" +
                 "\r\n" +
                 "<h2>Get Form</h2>\r\n" +
                 "<form method=\"get\" action=\"/form\">\r\n";
 
         tailResponse = "<input type=\"submit\" value=\"Submit\"/>\r\n" +
-                "</form>\r\n";
+                "</form>\r\n" +
+                "\r\n" +
+                "</html>\r\n";
 
-        boxText = new String[inputBoxes.length];
-        for (String box : inputBoxes) {
-            String line = "<label>" + box + "</label>\r\n" +
+        boxText = new String[boxes.length];
+        for (String box : boxes) {
+            String line = "<label>" + box + ":</label>\r\n" +
                     "<input type=\"text\" name=\"" + box.toLowerCase() + "\"/>\r\n";
             response = response + line;
         }
