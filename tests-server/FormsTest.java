9aa38b8a7b3b2d3e0d13199bf1248e56096a0dce
maniginam <maniginam@gmail.com>
2020-12-02 12:39:24 -0600

    form spec passing; still need post

--- tests-server/FormsTest.java
+++ tests-server/FormsTest.java
@@ -9,40 +9,37 @@ public class FormsTest {
 
     private HttpHandler handler;
     private HttpServer server;
-    private FormFactory formFactory;
-    private Form form;
+    private FormHandler form;
 
     @BeforeEach
     public void setup() throws IOException {
         handler = new HttpHandler(3141, "/Users/maniginam/server-task/http-spec/testroot");
         server = new HttpServer("/Users/maniginam/server-task/http-spec/testroot");
-        formFactory = new FormFactory();
     }
 
     @Test
-    public void withNoBoxes() throws IOException, ExceptionInfo {
-        String[] boxes = new String[0];
-        form = new Form(boxes);
+    public void formIsCalled() throws IOException, ExceptionInfo {
+        handler.handle("GET /forms.html HTTP/1.1");
+        String response = handler.getServer().getBodyMessage();
 
-        String instructions = form.getInstructions();
-        assertTrue(instructions.contains("<h2>Get Form</h2>"));
-        assertTrue(instructions.contains("<form method=\"get\" action=\"/form\""));
-        assertTrue(instructions.contains("<input type=\"submit\" value=\"Submit\""));
+        assertTrue(response.contains("<h2>Get Form</h2>"));
+        assertTrue(response.contains("<form method=\"get\" action=\"/form\""));
+        assertTrue(response.contains("<input type=\"submit\" value=\"Submit\""));
     }
 
     @Test
-    public void formWithOneInputBox() throws IOException, ExceptionInfo {
-        String[] boxes = new String[1];
-        boxes[0] = "Foo";
-        form = new Form(boxes);
-        String instructions = form.getInstructions();
+    public void oneFormEntry() throws IOException, ExceptionInfo {
+        handler.handle("GET /form?foo=1");
 
-        assertTrue(instructions.contains("<input type=\"text\" name=\"foo\""));
-        assertTrue(instructions.contains("<input type=\"submit\" value=\"Submit\""));
+        String body = handler.getServer().getBodyMessage();
+
+        assertTrue(body.contains("<h2>GET Form</h2>"));
+        assertTrue(body.contains("<li>foo: 1</li>"));
+        assertFalse(body.contains("<li>bar: 2</li>"));
     }
 
     @Test
-    public void formWithFooEntry() throws IOException, ExceptionInfo {
+    public void twoFormEntries() throws IOException, ExceptionInfo {
         String request = "GET /form?foo=1&bar=2";
         handler.handle(request);
 
@@ -53,18 +50,17 @@ public class FormsTest {
         assertTrue(body.contains("<li>bar: 2</li>"));
     }
 
-//    @Test
-//    public void formWithTwoInputBoxes() throws IOException, ExceptionInfo {
-//        String[] boxes = new String[2];
-//        boxes[0] = "Foo";
-//        boxes[1] = "Bar";
-//        form = new Form(boxes);
-//        String instructions = form.getInstructions();
-//
-//        assertTrue(instructions.contains("<input type=\"text\" name=\"foo\""));
-//        assertTrue(instructions.contains("<input type=\"text\" name=\"bar\""));
-//        assertTrue(instructions.contains("<input type=\"submit\" value=\"Submit\""));
-//    }
+    @Test
+    public void twoFormEntriesWithLongInputs() throws IOException, ExceptionInfo {
+        String request = "GET /form?foo=Rex&bar=Leo";
+        handler.handle(request);
+
+        String body = handler.getServer().getBodyMessage();
+
+        assertTrue(body.contains("<h2>GET Form</h2>"));
+        assertTrue(body.contains("<li>foo: Rex</li>"));
+        assertTrue(body.contains("<li>bar: Leo</li>"));
+    }
 
 
 }
